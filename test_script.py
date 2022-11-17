cd597db019a3b92fea095b8a38db2ad960bab59c
Jamel L. Hudson <74836109+Kvantman@users.noreply.github.com>
2022-11-16 22:34:26 -0500

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -5,8 +5,5 @@ Created on Wed Nov 16 22:14:48 2022
 @author: JLuca
 """
 
-def say_hello():
-    print("Hello man, be safe")
-    
-if __name__ == '__main__':
-    say_hello()
+def hello():
+  print("Hello world")
