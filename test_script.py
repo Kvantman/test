a93a0a2a74302b0857ac4fbd431d9a551700c397
Jamel L. Hudson <74836109+Kvantman@users.noreply.github.com>
2022-11-16 22:16:50 -0500

    Add files via upload

---
+++ test_script.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 16 22:14:48 2022
+
+@author: JLuca
+"""
+
+def say_hello():
+    print("Hello man, be safe")
+    
+if __name__ == '__main__':
+    say_hello()
